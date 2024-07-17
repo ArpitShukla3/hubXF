fbcf0b024334354ea74a5dae70a369c5db212535
ArpitShukla3 <arpit900shukla@gmail.com>
2024-07-17 20:41:24 +0530

    added

--- apiList.js
+++ apiList.js
@@ -1,5 +1,5 @@
 // export const server=`http://localhost:4000`
-export const server=`https://hubxb.onrender.com`
+export const server=`https://hubxb.onrender.com/api`
 
 export const apiRegister= `${server}/signup`
 export const apiLogin= `${server}/login`
